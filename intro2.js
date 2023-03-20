92a0d5d86432a52f69eb4dba15ca110b0399ff71
JingWang12345 <127298095+JingWang12345@users.noreply.github.com>
2023-03-20 13:28:19 +0100

    JS primo lezione

---
+++ intro2.js
@@ -0,0 +1,43 @@
+console.log('intro2');
+
+let pippo = 5;
+pippo = 10;
+console.log(pippo);
+
+//var pluto = 10;
+//pluto = 23;
+//console.log(pluto); il male!!!!
+
+const paperino = 20;
+// paperino = 17; non si può fare
+console.log(paperino);
+
+
+// console.log(clarabella);
+// var clarabella = 10;
+
+const topolino = 12 + 80;
+console.log(topolino);
+
+const basettoni  = 56;
+const gambaDilegno =basettoni;
+console.log(gambaDilegno);
+
+const trudy = gambaDilegno + basettoni;
+console.log(trudy);
+
+let qui = 12, quo = 12, qua = 12;
+console.log(qui, quo, qua);
+
+let paperoga;
+console.log(paperoga);
+
+// let 12pippo; non è possibile un nome di variabile che inizi con un numero
+// const ziopeperone; non è possibile una costante vuota
+
+
+let macchiaNera = 12;
+console.log(macchiaNera);
+macchiaNera = 27;
+console.log(macchiaNera);
+
