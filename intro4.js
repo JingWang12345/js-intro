65ea2b0d2131a0ee46ac9fc9dd3639dc127305b8
JingWang <wangjing061993@gmail.com>
2023-03-27 22:24:55 +0200

    Update intro4.js

--- intro4.js
+++ intro4.js
@@ -457,3 +457,129 @@
 // }
 
 // console.log(countSpaces('il nano enorme')) //2
+
+
+
+///--------------------------------------------------------------------------------------------------
+//ESERCIZI 27/03
+
+//1) scrivere una funzione che prende in input un array di numeri è restituisce un array di stringhe in logica FizzBuzz (map)
+
+// const numbers = [3, 5, 15, 8, 12, 2];
+
+// function convertArrayInFizzBuzz(array){
+
+//     const tempArray = [];
+//     for (let i = 0; i < array.length; i++) {
+//         const element = array[i];
+//         if (element % 3 === 0 && element % 5 === 0) {
+//             console.log('FizzBuzz');
+//         } else if (element % 3 === 0){
+//             console.log('Fizz');
+//         } else if (element % 5 === 0) {
+//             console.log('Buzz');
+//         } else {
+//             console.log(element);
+//         }
+   
+//     }
+//     return tempArray;
+// }
+
+
+// console.log(convertArrayInFizzBuzz(numbers)) //['Fizz', 'Buzz', 'FizzBuzz', '8', 'Fizz', '2']
+
+//2) scrivere una funzione che prende in input un array di numeri è restituisce un array di numeri in cui i positivi sono trasformati in negativi e viceversa (map)
+
+// const numbers1 = [-3, 5, 15, -8, 12, 2, 0];
+
+// function invertElements(array){
+//     const tempArray = [];
+//     for (let i = 0; i < array.length; i++) {
+//         const element = array[i];
+//         const risultato = element*-1;
+//         tempArray.push(risultato);
+//     }
+//     return tempArray;
+// }
+
+
+// console.log(invertElements(numbers1)) //[3, -5, -15, 8, -12, -2, 0]
+
+//3) scrivere una funzione che prende in input un array di stringhe è restituisce un array di numeri con la lunghezza delle stringhe (map)
+
+// const strings = ['pippo', 'pluto', 'qui'];
+
+// function lengthOfElements(array){
+//     const tempArray = [];
+//     for (let i = 0; i < array.length; i++) {
+//         const element = array[i];
+//         const risultato = element.length;
+//         tempArray.push(risultato);
+//     }
+//     return tempArray;
+// }
+
+// console.log(lengthOfElements(strings)) //[4, 5, 3]
+
+
+//4) scrivere una funzione che prende in input un array di stringhe è restituisce solo quelle che contengono la lettera z (filter)
+
+// const strings2 = ['pippo', 'pluto', 'qui', 'zapotec'];
+
+// function onlyElementsWithZ(array){
+//     const tempArray = [];
+//         for (let i = 0; i < array.length; i++) {
+//             const element = array[i];
+//             if (z[Symbol.match]){
+//                 tempArray.push(element);
+//             }
+//         } 
+//         return tempArray;
+//     }
+
+
+// console.log(onlyElementsWithZ(strings2)) //['zapotec']
+
+//5) scrivere una funzione che prende in input un array di numeri e restituisce tutti i numeri pari (filter)
+
+// const numbers2 = [-3, 5, 15, -8, 12, 2, 0];
+
+// function onlyEven(array){
+//     const tempArray = [];
+//         for (let i = 0; i < array.length; i++) {
+//             const element = array[i];
+//             if (element % 2 === 0){
+//                 tempArray.push(element);
+//             }
+//         } 
+//         return tempArray;
+// }
+
+// console.log(onlyEven(numbers2)) //[8, -12, -2, 0]
+
+//6) scrivere una funzione che prende in input un array di stringhe restituisce una stringa composta dalle iniziali
+//(reduce)
+// const strings3 = ['pippo', 'osvaldo', 'paperino'];
+
+// function sumFirstChar(array){
+
+// }
+
+// console.log(sumFirstChar(strings3)) //'pop'
+
+//7) scrivere una funzione che prende in input un array di numeri e restituisce il maggiore
+
+// const numbers3 = [-3, 5, 15, -8, 12, 2, 0];
+
+// function maxElement(array){
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     if (a>b) {
+        
+//     }
+    
+// }
+// }
+
+// console.log(maxElement(numbers3)) //15
